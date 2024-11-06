7c9e3a2278fd87b72b37bf4d9d2823439f3ba44d
Zack <zackagba7@gmail.com>
2024-11-06 20:04:49 +0100

    Fixes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
 export default function ContactUs() {
